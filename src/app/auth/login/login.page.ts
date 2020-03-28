4c2c98d319a8d07dac10bbc62768456512cbcbb7
Grzegorz Kędzierski <gk1999@interia.pl>
2020-03-28 16:23:36 +0100

    Added loading spinner to login page and alert

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,4 @@
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController, AlertController } from '@ionic/angular';
 import { ValidatorsService } from './../../shared/validators.service';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
@@ -23,7 +23,13 @@ export class LoginPage implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor(private authService: AuthService, private formBuilder: FormBuilder, private nav: NavController) { }
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private nav: NavController,
+    private loading: LoadingController,
+    private alert: AlertController
+  ) { }
 
   ngOnInit() {
     this.formGroup = this.formBuilder.group({
@@ -38,21 +44,86 @@ export class LoginPage implements OnInit {
     if (this.formGroup.invalid) {
       return;
     }
+    this.loading.create({ message: 'Logowanie...', spinner: 'dots' }).then(x => {
+      x.present();
 
-    this.authService.signInWithEmail('test@test.pl', 'password123').then(x => {
-      this.nav.navigateForward(['/home']);
-    }).catch(e => console.log(e));
+      this.authService.signInWithEmail(this.formGroup.controls.email.value, this.formGroup.controls.password.value).then(r => {
+        this.nav.navigateForward(['/home']);
+        x.dismiss();
+      }).catch(e => {
+        x.dismiss();
+        this.showLoginErrorMessage(e);
+      });
+    });
   }
 
   signInWithFacebook() {
-    this.authService.signInWithFacebook().then(x => {
-      this.nav.navigateForward(['/home']);
-    }).catch(e => console.log(e));
+    this.loading.create({ message: 'Logowanie...', spinner: 'dots' }).then(x => {
+      x.present();
+
+      this.authService.signInWithFacebook().then(r => {
+        this.nav.navigateForward(['/home']);
+        x.dismiss();
+      }).catch(e => {
+        x.dismiss();
+        this.showLoginErrorMessage(e);
+      });
+    });
   }
 
   signInWithGoogle() {
-    this.authService.signInWithGoogle().then(x => {
-      this.nav.navigateForward(['/home']);
-    }).catch(e => console.log(e));
+    this.loading.create({ message: 'Logowanie...', spinner: 'dots' }).then(x => {
+      x.present();
+
+      this.authService.signInWithGoogle().then(r => {
+        this.nav.navigateForward(['/home']);
+        x.dismiss();
+      }).catch(e => {
+        x.dismiss();
+        this.showLoginErrorMessage(e);
+      });
+    });
+  }
+
+  showLoginErrorMessage(error: any) {
+    let messageText = error.message;
+
+    switch (error.code) {
+      case 'auth/user-not-found':
+        messageText = 'Nie znaleziono użytkownika.';
+        break;
+      case 'auth/session-cookie-revoked':
+        messageText = 'Sesja została zakończona. Zaloguj się ponownie.';
+        break;
+      case 'auth/session-cookie-expired':
+        messageText = 'Sesja została zakończona. Zaloguj się ponownie.';
+        break;
+      case 'auth/invalid-password':
+        messageText = 'Podano niepoprawne hasło.';
+        break;
+      case 'auth/wrong-password':
+        messageText = 'Podano niepoprawne hasło.';
+        break;
+      case 'auth/invalid-credential':
+        messageText = 'Weryfikacja nie powiodła się.';
+        break;
+      case 'auth/too-many-requests':
+        messageText = 'Zbyt wiele nieudanych prób logowania. Prosimy spróbować później.';
+        break;
+    }
+
+    this.alert.create({
+      header: 'Niepowodzenie',
+      message: messageText,
+      buttons: ['Ok']
+    }).then(x => {
+      x.present().then(r => {
+        const btnOk = document.querySelector('.alert-button.ion-focusable.ion-activatable') as any;
+
+        if (btnOk) {
+          btnOk.focus();
+        }
+      });
+    });
   }
 }
