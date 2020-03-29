205297457ddd07fef9dcfb9b422111ad5129ede9
Grzegorz Kędzierski <gk1999@interia.pl>
2020-03-29 18:03:26 +0200

    Working on UserService

--- src/app/shared/services/user/user.service.spec.ts
+++ src/app/shared/services/user/user.service.spec.ts
@@ -1,9 +1,13 @@
+import { DocumentReference } from '@angular/fire/firestore/interfaces';
+import { AppUser } from './../../../auth/app-user.model';
+import { of } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from './../../../auth/auth.service';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from 'firebase';
 
 fdescribe('UserService', () => {
   let authServiceSpyObj: jasmine.SpyObj<AuthService>;
@@ -26,11 +30,291 @@ fdescribe('UserService', () => {
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
-    console.log(service);
     expect(service).toBeTruthy();
   });
 
   describe('checkIfWasUdpatedFirestoreData', () => {
+    it('nothing from firestore was changed should return false', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
 
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFirestoreData(user);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('schoolRef was changed should return true', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      user.schoolRef = { id: 'test2' } as DocumentReference;
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFirestoreData(user);
+
+      expect(result).toBeTruthy();
+    });
+
+    it('subscriptionDateEnd was changed should return true', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      user.subscriptionDateEnd = new Date();
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFirestoreData(user);
+
+      expect(result).toBeTruthy();
+    });
+
+    it('subscriptionDateEnd was changed to the same value should return false', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      user.subscriptionDateEnd = new Date('2020-01-01');
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFirestoreData(user);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('firebaseUser was changed should return false', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      user.firebaseUser = { uid: 'test' } as User;
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFirestoreData(user);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('authService returns null user should return false', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(null)
+      });
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFirestoreData(user);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('authService throws should return false', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(Promise.reject())
+      });
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFirestoreData(user);
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('checkIfWasUdpatedFiresAutheData', () => {
+    it('nothing from fireAuth was changed should return false', async () => {
+      let user = {
+        firebaseUser: { uid: 'test' } as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFireAutheData(user);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('uid from fireAuth was changed should return true', async () => {
+      let user = {
+        firebaseUser: { uid: 'test' } as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      user.firebaseUser.uid = 'new value';
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFireAutheData(user);
+
+      expect(result).toBeTruthy();
+    });
+
+    it('displayName from fireAuth was changed should return true', async () => {
+      let user = {
+        firebaseUser: { uid: 'test' } as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      user.firebaseUser.displayName = 'new value';
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFireAutheData(user);
+
+      expect(result).toBeTruthy();
+    });
+
+    it('schoolRef from firestore was changed should return false', async () => {
+      let user = {
+        firebaseUser: { uid: 'test' } as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(JSON.parse(JSON.stringify(user)))
+      });
+
+      user.schoolRef = { id: 'test231' } as DocumentReference;
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFireAutheData(user);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('authService returns null user should return false', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(null)
+      });
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFireAutheData(user);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('authService throws should return false', async () => {
+      let user = {
+        firebaseUser: {} as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+
+      Object.defineProperty(authServiceSpyObj, 'getCurrentUser', {
+        value: of(Promise.reject())
+      });
+
+      const service = TestBed.get(UserService) as UserService;
+
+      const result = await service.checkIfWasUdpatedFireAutheData(user);
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('updateUser', () => {
+    let user: AppUser;
+    beforeEach(() => {
+      user = {
+        firebaseUser: { uid: 'test' } as User,
+        schoolRef: { id: 'test' } as DocumentReference,
+        subscriptionDateEnd: new Date('2020-01-01')
+      } as AppUser;
+    });
+
+    it('all user data was changed and succesfuly updated should return observable', async () => {
+      const service = TestBed.get(UserService) as UserService;
+
+      spyOn(service, 'checkIfWasUdpatedFireAutheData').and.returnValue(Promise.resolve(true));
+      spyOn(service, 'checkIfWasUdpatedFirestoreData').and.returnValue(Promise.resolve(true));
+
+      firestoreSpyObj.collection.and.returnValue({
+        doc: () => ({
+          update: () => Promise.resolve()
+        })
+      } as any);
+
+      fireAuthSpyObj.updateCurrentUser.and.returnValue(Promise.resolve());
+      authServiceSpyObj.refreshUserData.and.returnValue(Promise.resolve(of(user)));
+
+      const result = await service.updateUser(user);
+
+      expect(result).toBeTruthy();
+      expect(firestoreSpyObj.collection).toHaveBeenCalled();
+      expect(fireAuthSpyObj.updateCurrentUser).toHaveBeenCalled();
+    });
   });
 });
--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,8 +1,10 @@
+import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from './../../../auth/auth.service';
 import { AppUser } from './../../../auth/app-user.model';
 import { Injectable } from '@angular/core';
+import { User } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +13,58 @@ export class UserService {
 
   constructor(private authService: AuthService, private fireAuth: AngularFireAuth, private firestore: AngularFirestore) { }
 
-  updateUserData(updatedUser: AppUser): Promise<void> {
-    return new Promise((resolve, reject) => {
+  async updateUser(updatedUser: AppUser): Promise<Observable<AppUser>> {
+    if (await this.checkIfWasUdpatedFirestoreData(updatedUser)) {
+      const dataToUpdate = JSON.parse(JSON.stringify(updatedUser)) as AppUser;
+      delete dataToUpdate.firebaseUser;
 
-    });
+      await this.firestore.collection('users').doc(updatedUser.firebaseUser.uid).update(dataToUpdate);
+    }
+
+    if (await this.checkIfWasUdpatedFireAutheData(updatedUser)) {
+      await this.fireAuth.updateCurrentUser(updatedUser.firebaseUser);
+    }
+
+    return await this.authService.refreshUserData();
   }
 
-  checkIfWasUdpatedFirestoreData(updatedUser: AppUser): boolean {
-    return true;
+  async checkIfWasUdpatedFireAutheData(updatedUser: AppUser): Promise<boolean> {
+    try {
+      const currentUser = await this.authService.getCurrentUser.toPromise();
+
+      if (!currentUser) {
+        throw new Error('User is not logged in');
+      }
+
+      const currentUserOnlyFireAuth = { ...currentUser.firebaseUser } as User;
+
+      const updatedUserOnlyFireAuth = { ...updatedUser.firebaseUser } as User;
+
+      return JSON.stringify(currentUserOnlyFireAuth) !== JSON.stringify(updatedUserOnlyFireAuth);
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  async checkIfWasUdpatedFirestoreData(updatedUser: AppUser): Promise<boolean> {
+    try {
+      const currentUser = await this.authService.getCurrentUser.toPromise();
+
+      if (!currentUser) {
+        throw new Error('User is not logged in');
+      }
+
+      const currentUserWithoutFireAuth = { ...currentUser } as AppUser;
+      delete currentUserWithoutFireAuth.firebaseUser;
+
+      const updatedUserWithoutFireAuth = { ...updatedUser } as AppUser;
+      delete updatedUserWithoutFireAuth.firebaseUser;
+
+      return JSON.stringify(currentUserWithoutFireAuth) !== JSON.stringify(updatedUserWithoutFireAuth);
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 }
